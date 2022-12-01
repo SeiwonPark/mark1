8317a210b35b99e4731d6ccc4bf6fb5657042bff
SeiwonPark <psw7347@gmail.com>
2022-12-01 17:18:51 +0900

    fix: moved google analytics configuration inside themeConfig

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,10 +37,6 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
-        googleAnalytics: {
-          trackingID: process.env.REACT_APP_TRACKING_ID,
-          anonymizeIP: true,
-        },
       }),
     ],
   ],
@@ -109,6 +105,10 @@ const config = {
         // externalUrlRegex: "google\\.com|naver\\.com|tistory\\.com",
         searchParameters: {},
         searchPagePath: "search",
+      },
+      googleAnalytics: {
+        trackingID: process.env.REACT_APP_TRACKING_ID,
+        anonymizeIP: true,
       },
     }),
   /**
